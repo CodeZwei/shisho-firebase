160df4f30b1fecc25804f70a0987304cd3992b4b
CodeZwei <brandonzwei@gmail.com>
2022-07-23 18:20:10 -0700

    Adds differential to initialize fire admin from env var instead of file

--- client/src/lib/firebase/server.ts
+++ client/src/lib/firebase/server.ts
@@ -1,6 +1,23 @@
-import {initializeApp} from 'firebase-admin';
+import * as fbAdmin from 'firebase-admin';
+const {initializeApp, credential} = fbAdmin;
 
-const admin = initializeApp();
+// Initializes Firebase admin
+// https://dev.to/vvo/how-to-add-firebase-service-account-json-files-to-vercel-ph5
+
+let admin: fbAdmin.app.App;
+
+if (process.env.FIREBASE_ADMIN_CREDENTIALS) {
+    // Credentials loaded through Environment Variable
+    const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS as string);
+
+    admin = initializeApp({
+        credential: credential.cert(serviceAccount),
+    });
+} else {
+    // Credentials loaded from file saved in GOOGLE_APPLICATION_CREDENTIALS
+    admin = initializeApp();
+}
+
 const db = admin.firestore();
 const auth = admin.auth();
 
