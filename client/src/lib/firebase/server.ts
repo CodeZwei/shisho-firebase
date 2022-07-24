e52a307372a6396e4f9d480827359f617b002eeb
CodeZwei <brandonzwei@gmail.com>
2022-07-23 18:33:46 -0700

    Converted to commonjs style import for firebase-admin

--- client/src/lib/firebase/server.ts
+++ client/src/lib/firebase/server.ts
@@ -1,20 +1,23 @@
-import * as admin from 'firebase-admin';
+/**
+ * @fileoverview
+ * Initializes Firebase admin
+ * https://dev.to/vvo/how-to-add-firebase-service-account-json-files-to-vercel-ph5
+ **/ 
 
-// Initializes Firebase admin
-// https://dev.to/vvo/how-to-add-firebase-service-account-json-files-to-vercel-ph5
+import fbAdmin from 'firebase-admin';
 
-let app: admin.app.App;
+let app: fbAdmin.app.App;
 
 if (process.env.FIREBASE_ADMIN_CREDENTIALS) {
     // Credentials loaded through Environment Variable
     const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS as string);
 
-    app = admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+    app = fbAdmin.initializeApp({
+        credential: fbAdmin.credential.cert(serviceAccount),
     });
 } else {
     // Credentials loaded from file saved in GOOGLE_APPLICATION_CREDENTIALS
-    app = admin.initializeApp();
+    app = fbAdmin.initializeApp();
 }
 
 const db = app.firestore();
